fa92f594ab741b3db720b55b68fe12da123dc8fc
Samuel Galo <samuelgalo1625@gmail.com>
2024-03-10 15:28:02 -0600

    Commit Agregar Lista
    Aun no aparece un arreglo de listas en la pagina ToDo pero aparecen en la consola

---
+++ proyecto-pwi/src/components/ToDoForm.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+const ToDoForm = ({addTodo}) => {
+
+const [value, setValue] = useState('');
+
+const handleSubmit = e => {
+    e.preventDefault();
+
+    addTodo(value);
+
+    setValue('');
+}
+
+  return (
+    <>
+     <form action="" className="TodoForm" onSubmit={handleSubmit}>
+     <input type="text" className='todo-input' placeholder='¿Que tienes pensando hacer hoy?' onChange={(e) =>setValue(e.target.value)}/>
+     <button type='submit' className='todo-btn'>Añadir Tarea</button>   
+    </form> 
+    </>
+  )
+}
+
+export default ToDoForm
---
+++ proyecto-pwi/src/components/ToDoWrapper.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import ToDoForm from './ToDoForm'
+import {v4 as uuidv4} from 'uuid'
+uuidv4();
+
+const ToDoWrapper = () => {
+
+const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+       setTodos([...todos,{id: uuidv4(), task: todo, completed: false,
+    isEditing: false}])
+    console.log(todos);
+    }
+
+  return (
+    <div className='TodoWrapper'>
+    <ToDoForm addTodo={addTodo}/>
+    </div>
+  )
+}
+
+export default ToDoWrapper
